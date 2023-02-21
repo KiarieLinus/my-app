53889e05c96d16ae0249e92cf38087a7363b28ec
KiarieLinus <kiarielinus@gmail.com>
2023-02-22 01:15:07 +0300

    push

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React-Native
+          Learn React with Linus
         </a>
       </header>
     </div>
