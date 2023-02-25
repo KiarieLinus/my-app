955fb98770638613c21b2c10dc8361348702cf3a
KiarieLinus <kiarielinus@gmail.com>
2023-02-25 18:46:28 +0300

    Setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div >
-      <img src={logo} className="App-logo" alt="logo" />
-      <div className="List">
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100k stars on Github</li>
-          <li>Is maintained by Facebook</li>
-          <li>Power thousands of enerprise apps, including mobile apps</li>
-        </ul>
-      </div>
+    <div className='container'>
+      <Navbar />
+      <Main />
     </div>
   );
 }
