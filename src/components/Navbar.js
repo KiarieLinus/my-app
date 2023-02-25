8640804ae5da00500877fb39cae687fcf44333d9
KiarieLinus <kiarielinus@gmail.com>
2023-02-26 00:00:42 +0300

    Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
+import logo from '../images/logo.svg'
+
 export default function Navbar(){
     return(
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav>
+            <img src={logo}/>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
     )
 }
